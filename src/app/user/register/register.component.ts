2701563db48ab0efb01ac524df266481f26b50d0
Brian De Sousa <briandesousa1@gmail.com>
2018-12-28 02:09:05 -0500

    prepare registration reactive form with bootstrap styling

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  registrationForm = new FormGroup({
+    emailAddress: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+  
   constructor() { }
 
+  ngOnInit() { }
+
+  onRegistrationSubmit() { }
 }
