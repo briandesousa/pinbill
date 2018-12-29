243a2dad1f9383641b290820a9e922f47c5bd6d3
Brian De Sousa <briandesousa1@gmail.com>
2018-12-29 01:31:03 -0500

    initial implementation for register component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,22 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
   registrationForm = new FormGroup({
     emailAddress: new FormControl(''),
     password: new FormControl(''),
     confirmPassword: new FormControl('')
   });
+
+  private unsubscribe: Subject<void> = new Subject();
   
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.afAuth.authState
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((user: firebase.User) => {
+        if(user) {
+          // user is already logged in
+          this.router.navigate(['home']);
+        } 
+      });
+  }
 
-  onRegistrationSubmit() { }
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  onRegistrationSubmit() { 
+    let email = this.registrationForm.value.emailAddress;
+    let password = this.registrationForm.value.password;
+
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then( (value: firebase.auth.UserCredential) => {
+
+      })
+      .catch( reason => {
+        let errorCode = reason.code;
+        let errorMessage = reason.message;
+        console.log(`Error Code: ${errorCode}, Message: ${errorMessage}`);
+      });
+  }
 }
