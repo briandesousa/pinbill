91dabd5aa2d02bde19b5cfec59d2b3e31ef92ce7
Brian De Sousa <briandesousa1@gmail.com>
2018-12-29 01:32:12 -0500

    display user info, status and login/logout actions in app header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pinbill';
+  $isLoggedIn: Observable<boolean>;
+  $user: Observable<User>;
+
+  constructor(
+    private userService: UserService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.$isLoggedIn = this.userService.isLoggedIn();
+    this.$user = this.userService.getUser();
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['user', 'login']);
+  }
 }
