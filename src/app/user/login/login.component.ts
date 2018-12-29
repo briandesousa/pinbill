f0f2d113be5d650280ced04f7d08b5f4687c6210
Brian De Sousa <briandesousa1@gmail.com>
2018-12-29 01:30:09 -0500

    switch login component to UserService for authentication

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,34 +1,39 @@
-import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { User } from '../user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl('')
   });
 
-  constructor(
-    public afAuth: AngularFireAuth,
-    private userService: UserService) { }
+  $user: Observable<User>;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.$user = this.userService.getUser();
+  }
 
   loginWithGoogle() {
-    this.afAuth.auth.signInWithPopup(this.userService.getGoogleAuthProvider());
+    this.userService.loginWithGoogle();
   }
 
   loginWithEmail() {
-    this.afAuth.auth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password);
+    this.userService.loginWithEmail(this.loginForm.value.email, this.loginForm.value.password);
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.userService.logout();
   }
 
 }
